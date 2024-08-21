4467de99b83d7e46e927d2d31a11a4607761fdad
Laesp <44760628+Laesp@users.noreply.github.com>
2024-08-21 20:35:29 +0200

    Update n00dles.js

--- n00dles.js
+++ n00dles.js
@@ -1,7 +1,7 @@
 /** @param {NS} ns */
 export async function main(ns) {
   let targetServer = 'n00dles';
-
+,
 	while(true) {
     if ( (ns.getServerSecurityLevel(targetServer) - ns.getServerMinSecurityLevel(targetServer) ) > 0.05 ) {
       await ns.weaken(targetServer)
